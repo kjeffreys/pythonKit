84987b4de92bb10111ef88ae0532fbd9142e9168
kjeffreys <kajeffr1@asu.edu>
2020-05-29 17:55:49 -0400

    Added elementwise methods to demo np divide/exponent/comparators(boolean results)

--- numpy/matrixArithmetic.py
+++ numpy/matrixArithmetic.py
@@ -24,11 +24,31 @@ def getIdentyMatrixMult(dimSize):
     print("*" * 50)
     print(np.identity(dimSize))
 
+def elementWiseDivide(arr=exampleMatrix, dividend=1):
+    print("*" * 50)
+    print("{} / arr = 1 /\n({}) =\n".format(dividend, arr))
+    return dividend / arr
+
+def elementWiseExponent(arr=exampleMatrix, exponent=2):
+    print("*" * 50)
+    print("arr ** {} = ({})\n(to the power {}) =\n".format(exponent, arr, exponent))
+    return (arr ** exponent)
+
+def booleanArrayComparison(arr1=exampleMatrix, arr2=np.array([ [4,2,1], [1,6,20] ])):
+    print("*" * 50)
+    print("arr1 = {}".format(arr1))
+    print("arr2 = {}".format(arr2))
+    print("arr1 < arr2 =\n")
+    return arr1 < arr2
+
 if __name__ == "__main__":
     print(elementWiseMultiply())
     print(elementWiseAdd())
     print(identityMultiply())
     getIdentyMatrixMult(3)
+    print(elementWiseDivide())
+    print(elementWiseExponent())
+    print(booleanArrayComparison())
     
 
 
