def7ab0a6933d47714a51ca9e3c28d4dcf06db81
kjeffreys <kajeffr1@asu.edu>
2020-06-03 18:06:11 -0400

    Broke transposing/axes into 2 files. Fns demoing numpy matrix transposing/ways to change axes

--- numpy/transposingNumpyArrays.py
+++ numpy/transposingNumpyArrays.py
@@ -18,7 +18,16 @@ def permuteAxes(arr=np.arange(16).reshape(2,2,4)):
     print(arr)
     print("(transpose) ->")
     return arr.transpose()
-    
+
+def reorderAxes(arr=np.arange(16).reshape(2,2,4)):
+    print(arr)
+    print("reordering axes (1,0,2):")
+    return arr.transpose(1,0,2) # (dim0, dim1, dim2) -> (dim1, dim0, dim2)
+
+def swapAxes(arr=np.arange(16).reshape(2,2,4)):
+    print(arr)
+    print("swapaxes (1,2):")
+    return arr.swapaxes(1,2)    
 
 if __name__ == "__main__":
     print(getArr())
@@ -28,4 +37,8 @@ if __name__ == "__main__":
     print(dotProduct())
     print("-"*15)
     print(permuteAxes())
+    print("-"*15)
+    print(reorderAxes())
+    print("-"*15)
+    print(swapAxes())
 
