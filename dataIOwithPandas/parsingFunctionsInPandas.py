603d4ee1bc9d17ed08c6344742f9398c777e5732
kjeffreys <kajeffr1@asu.edu>
2020-06-15 12:10:53 -0400

    parsingFunctionsInPandas.py pandas methods to convert different file types into pandas DataFrame obj

---
+++ dataIOwithPandas/parsingFunctionsInPandas.py
@@ -0,0 +1,36 @@
+'''
+Table: The following are pandas methods to read from various file types.
+They convert text data into a DataFrame.
+============================================================================================
+Function------------Description-------------------------------------------------------------
+============================================================================================
+read_csv            Load delimited data from a file, URL, or file-like obj; use comma as 
+                    default delimeter
+============================================================================================
+read_fwf            Read data in fixed-width column format (i.e. no delimeters)
+============================================================================================
+read_clipboard      Version of read_csv that reads data from the clipboard; useful for 
+                    converting tables from web pages.
+============================================================================================
+read_excel          Read tabular data from an Excel XLS or XLSX file
+============================================================================================
+read_hdf            Read HDF5 files written by pandas
+============================================================================================
+read_html           Read all tables found in the given HTML document
+============================================================================================
+read_json           Read data from a JSON string representation
+============================================================================================
+read_msgpack        Read pandas data encoded using the Message-Pack binary format
+============================================================================================
+read_pickle         Read an arbitrary object stored in Python pickle format
+============================================================================================
+read_sas            Read a SAS dataset stored in one of the SAS system's custom storage
+                    formats
+============================================================================================
+read_sql            Read the results of a SQL query (using SQLAlchemy) as a pandas DataFrame
+============================================================================================
+read_stata          Read a dataset from the Stata file format
+============================================================================================
+read_feather        Read the Feather binary file format
+============================================================================================
+'''
