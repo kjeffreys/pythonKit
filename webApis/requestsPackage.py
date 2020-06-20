fc10af59fe5da726c5e6ddd1346f6a6b2c667367
kjeffreys <kajeffr1@asu.edu>
2020-06-19 22:52:53 -0400

    rP.py made github api query to pandas-dev issues page with requests package, stored issues into DataFrame and displayed

--- webApis/requestsPackage.py
+++ webApis/requestsPackage.py
@@ -3,6 +3,7 @@ Issues came up when using pandas.read_html() due to encoding differences between
 (namely negative numbers '-' symbol was not recognized). Instead of dealing with encoding/decoding/re.sub()
 fixes, it is also possible to get around issues with the requests package.
 '''
+import pandas as pd
 import requests
 
 '''
@@ -10,18 +11,32 @@ This method displays a query usings the requests package to find
 the last 30 GitHub issues for pandas and Github with a 'GET' http request
 '''
 def last30Pandas():
-    url = 'https://api/github.com/repos/pandas-dev/pandas/issues'
+    # requests limit already reached, problem with repo?
+    url = 'https://api.github.com/repos/pandas-dev/pandas/issues'
 
     resp = requests.get(url)
 
     # resp.json() returns a dictionary containing JSON parsed into native Python objects
     data = resp.json()
 
-    for i, v in enumerate(data):
-        print("Issues {}:".format(i))
-        print(v['title'])
+    for i in range(30):
+        print("=" * 75)
+        print("Issue {}:".format(i))
+        print("=" * 75)
+        print(data[i]['title'])
 
-    return resp.headers
+    return data
+
+'''
+Each elements in the data from the requests query above is a dictionary containing
+all of the data found on a GitHub issue page (except for the comments). This can
+be passed directly to a DataFrame and subsequently fields of interest can be extracted.
+'''
+def extractFields():
+    issues = pd.DataFrame(last30Pandas(), columns=['number','title','labels','state'])
+
+    return issues
 
 if __name__ == "__main__":
-    print(last30Pandas())
+    #print(last30Pandas())
+    print(extractFields())
